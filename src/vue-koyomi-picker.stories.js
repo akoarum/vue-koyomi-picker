b61b5a85811ea0c5f7d8d5ff2a23c7d16741d917
Kenty Akoarum <kamome-black@akoarum.net>
2022-08-31 23:29:26 +0900

    update stories file

--- src/vue-koyomi-picker.stories.js
+++ src/vue-koyomi-picker.stories.js
@@ -4,17 +4,67 @@ export default {
   title: 'VueKoyomiPicker',
   component: VueKoyomiPicker,
   argTypes: {
+    to: {
+      control: { type: 'date' },
+      description:
+        'Specifies the maximum date and time that can be selected; the picker will not be able to select any later date and time.',
+    },
+    from: {
+      control: { type: 'date' },
+      description:
+        'Specifies the minimum date and time that can be selected; the picker will not be able to select earlier dates and times.',
+    },
+    defaultDate: {
+      control: { type: 'date' },
+      description: 'Specify the date and time that is selected when the picker is opened.',
+    },
+    format: {
+      control: { type: 'text' },
+      description: 'Specifies the format of the string.<br />See `format` in date-fns documentation.',
+    },
     onlyDate: {
       control: { type: 'boolean' },
+      description: 'Hide Time Picker and enabled Date Picker only.',
+    },
+    disabledDates: {
+      control: { type: 'object' },
+      description: 'Specify dates to disable.',
+    },
+    disabledDays: {
+      control: { type: 'object' },
+      description: 'Specify days of the week to disable.',
+    },
+    disabledHours: {
+      control: { type: 'object' },
+      description: 'Specify hours to disable.',
+    },
+    stepMinutes: {
+      control: { type: 'number', min: 1, max: 59 },
+      description: 'Specify the number of minute steps.',
+    },
+    firstDay: {
+      control: { type: 'select' },
+      options: [0, 1, 2, 3, 4, 5, 6],
+      description:
+        'Specify the first day of the week.<br />It is not linked to dayNames and needs to be reordered accordingly.',
+    },
+    dayNames: {
+      control: { type: 'object' },
+      description:
+        'Specify the name of the day of the week to be displayed in the calendar.<br />It is not linked to firstDay and needs to be reordered accordingly.',
+    },
+    teleportTo: {
+      control: { type: 'text' },
+      description: 'Specifies the teleport destination.<br />See Vue3 documentation for `teleport` .',
     },
   },
 }
 
-const Template = (args) => ({
+const BasicTemplate = (args) => ({
   components: { VueKoyomiPicker },
   data() {
     return {
-      value: new Date('2022/08/25'),
+      value: null,
     }
   },
   setup() {
@@ -24,21 +74,35 @@ const Template = (args) => ({
   },
   template: `
     <div>
-      <p>state: {{ value }}</p>
+      <VueKoyomiPicker v-model="value">
+        <template #activator="{ on, formattedValue }">
+          <input :value="formattedValue" type="text" @focus="on" />
+        </template>
+      </VueKoyomiPicker>
+    </div>
+  `,
+})
+
+export const Basic = BasicTemplate.bind({})
+
+const RangeTemplate = (args) => ({
+  components: { VueKoyomiPicker },
+  data() {
+    return {
+      value: null,
+    }
+  },
+  setup() {
+    return {
+      args,
+    }
+  },
+  template: `
+    <div>
       <VueKoyomiPicker
         v-model="value"
-        :to="args.to"
-        :from="args.from"
-        :default-date="args.defaultDate"
-        :format="args.format"
-        :only-date="args.onlyDate"
-        :disabled-dates="args.disabledDates"
-        :disabled-days="args.disabledDays"
-        :disabled-hours="args.disabledHours"
-        :step-minutes="args.stepMinutes"
-        :start-day="args.startDay"
-        :day-names="args.dayNames"
-        :teleport-to="args.teleportTo"
+        :to="new Date(args.to)"
+        :from="new Date(args.from)"
       >
         <template #activator="{ on, formattedValue }">
           <input :value="formattedValue" type="text" @focus="on" />
@@ -48,18 +112,178 @@ const Template = (args) => ({
   `,
 })
 
-export const Default = Template.bind({})
-Default.args = {
-  to: new Date('2022/10/15 18:45'),
-  from: new Date('2020/04/01 09:30'),
-  defaultDate: new Date(),
-  format: 'yyyy/MM/dd HH:mm',
-  onlyDate: false,
-  disabledDates: [],
-  disabledDays: [],
-  disabledHours: [],
-  stepMinutes: 1,
-  startDay: 0,
-  dayNames: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-  teleportTo: 'body',
+export const Range = RangeTemplate.bind({})
+Range.args = {
+  to: new Date('2023/04/30 11:00'),
+  from: new Date('2021/10/01 19:30'),
+}
+
+const OnlyDateTemplate = (args) => ({
+  components: { VueKoyomiPicker },
+  data() {
+    return {
+      value: null,
+    }
+  },
+  setup() {
+    return {
+      args,
+    }
+  },
+  template: `
+    <div>
+      <VueKoyomiPicker v-model="value" :only-date="args.onlyDate">
+        <template #activator="{ on, formattedValue }">
+          <input :value="formattedValue" type="text" @focus="on" />
+        </template>
+      </VueKoyomiPicker>
+    </div>
+  `,
+})
+
+export const OnlyDate = OnlyDateTemplate.bind({})
+OnlyDate.args = {
+  onlyDate: true,
+}
+
+const DisabledDatesTemplate = (args) => ({
+  components: { VueKoyomiPicker },
+  data() {
+    return {
+      value: null,
+    }
+  },
+  setup() {
+    return {
+      args,
+    }
+  },
+  template: `
+    <div>
+      <VueKoyomiPicker v-model="value" :default-date="args.defaultDate" :disabled-dates="args.disabledDates">
+        <template #activator="{ on, formattedValue }">
+          <input :value="formattedValue" type="text" @focus="on" />
+        </template>
+      </VueKoyomiPicker>
+    </div>
+  `,
+})
+
+export const DisabledDates = DisabledDatesTemplate.bind({})
+DisabledDates.args = {
+  defaultDate: new Date('2022/10/01'),
+  disabledDates: [new Date('2022/10/05'), new Date('2022/10/27')],
+}
+
+const DisabledDaysTemplate = (args) => ({
+  components: { VueKoyomiPicker },
+  data() {
+    return {
+      value: null,
+    }
+  },
+  setup() {
+    return {
+      args,
+    }
+  },
+  template: `
+    <div>
+      <VueKoyomiPicker v-model="value" :disabled-days="args.disabledDays">
+        <template #activator="{ on, formattedValue }">
+          <input :value="formattedValue" type="text" @focus="on" />
+        </template>
+      </VueKoyomiPicker>
+    </div>
+  `,
+})
+
+export const DisabledDays = DisabledDaysTemplate.bind({})
+DisabledDays.args = {
+  disabledDays: [1, 4],
+}
+
+const DisabledHoursTemplate = (args) => ({
+  components: { VueKoyomiPicker },
+  data() {
+    return {
+      value: null,
+    }
+  },
+  setup() {
+    return {
+      args,
+    }
+  },
+  template: `
+    <div>
+      <VueKoyomiPicker v-model="value" :disabled-hours="args.disabledHours">
+        <template #activator="{ on, formattedValue }">
+          <input :value="formattedValue" type="text" @focus="on" />
+        </template>
+      </VueKoyomiPicker>
+    </div>
+  `,
+})
+
+export const DisabledHours = DisabledHoursTemplate.bind({})
+DisabledHours.args = {
+  disabledHours: [0, 1, 2, 10, 22],
+}
+
+const StepMinutesTemplate = (args) => ({
+  components: { VueKoyomiPicker },
+  data() {
+    return {
+      value: null,
+    }
+  },
+  setup() {
+    return {
+      args,
+    }
+  },
+  template: `
+    <div>
+      <VueKoyomiPicker v-model="value" :step-minutes="args.stepMinutes">
+        <template #activator="{ on, formattedValue }">
+          <input :value="formattedValue" type="text" @focus="on" />
+        </template>
+      </VueKoyomiPicker>
+    </div>
+  `,
+})
+
+export const StepMinutes = StepMinutesTemplate.bind({})
+StepMinutes.args = {
+  stepMinutes: 15,
+}
+
+const CustomDaysTemplate = (args) => ({
+  components: { VueKoyomiPicker },
+  data() {
+    return {
+      value: null,
+    }
+  },
+  setup() {
+    return {
+      args,
+    }
+  },
+  template: `
+    <div>
+      <VueKoyomiPicker v-model="value" :first-day="args.firstDay" :day-names="args.dayNames">
+        <template #activator="{ on, formattedValue }">
+          <input :value="formattedValue" type="text" @focus="on" />
+        </template>
+      </VueKoyomiPicker>
+    </div>
+  `,
+})
+
+export const CustomDays = CustomDaysTemplate.bind({})
+CustomDays.args = {
+  firstDay: 1,
+  dayNames: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
 }
